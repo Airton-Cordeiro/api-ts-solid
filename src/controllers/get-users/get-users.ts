195162416cd47e6305321095115e53bb5a0121c5
Airton Cordeiro <airton.acds@gmail.com>
2024-08-09 00:19:05 -0300

    add models and repository

---
+++ src/controllers/get-users/get-users.ts
@@ -0,0 +1,24 @@
+import { IGetUsersController, IGetUsersRepository } from "./protocols";
+
+export class GetUsersController implements IGetUsersController {
+  constructor(private readonly getUsersRepository: IGetUsersRepository) {}
+
+  async handle() {
+    //validar requisicao
+
+    //redirecionar chamada para o repository
+    try {
+      const users = await this.getUsersRepository.getUsers();
+
+      return {
+        statusCode: 200,
+        body: users,
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: "Something Went Wrong.",
+      };
+    }
+  }
+}
