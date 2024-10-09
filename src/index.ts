e16eb62f3435d065e7919af3e6ffbb369cce273f
Airton Cordeiro <airton.acds@gmail.com>
2024-10-08 22:11:04 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log(" teste ");
+import express from "express";
+import { config } from "dotenv";
+import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-users";
+import { GetUsersController } from "./controllers/get-users/get-users";
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.get("/users", async (req: any, res: any) => {
+  const mongoGetUsersRepository = new MongoGetUsersRepository();
+  const getUsersController = new GetUsersController(mongoGetUsersRepository);
+
+  const { body, statusCode } = await getUsersController.handle();
+
+  res.send(body).status(statusCode);
+});
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
