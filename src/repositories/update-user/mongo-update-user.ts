bddd67c195112a69d8df5edf3d74b5981e9ca047
Airton Cordeiro <airton.acds@gmail.com>
2024-10-10 22:11:04 -0300

    feat: add updateUserController with validations

--- src/repositories/update-user/mongo-update-user.ts
+++ src/repositories/update-user/mongo-update-user.ts
@@ -2,12 +2,12 @@ import { ObjectId } from "mongodb";
 import {
   IUpdateUserRepository,
   UpdateUserParams,
-} from "../../controllers/update-user/update-user";
+} from "../../controllers/update-user/protocols";
 import { MongoClient } from "../../database/mongo";
 import { User } from "../../models/user";
 
 export class MongoUpdateUserRepository implements IUpdateUserRepository {
-  async updateUsers(id: string, params: UpdateUserParams): Promise<User> {
+  async updateUser(id: string, params: UpdateUserParams): Promise<User> {
     await MongoClient.db.collection("users").updateOne(
       { _id: new ObjectId(id) },
       {
@@ -17,7 +17,7 @@ export class MongoUpdateUserRepository implements IUpdateUserRepository {
       }
     );
     const user = await MongoClient.db
-      .collection<Omit<User, "id">>("user")
+      .collection<Omit<User, "id">>("users")
       .findOne({ _id: new ObjectId(id) });
 
     if (!user) {
